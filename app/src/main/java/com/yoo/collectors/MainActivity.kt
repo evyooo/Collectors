410be8d0a04d3a1a8ed7fa4b78b378cb565a2328
evyooo <yooooojining@gmail.com>
2022-02-02 02:15:23 +0900

    feat(main): #1 프래그먼트 연결

--- app/src/main/java/com/yoo/collectors/MainActivity.kt
+++ app/src/main/java/com/yoo/collectors/MainActivity.kt
@@ -2,10 +2,41 @@ package com.yoo.collectors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.adapters.AdapterViewBindingAdapter.setOnItemSelectedListener
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.yoo.collectors.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        navigationItemSelect()
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.holder_fl_main, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun navigationItemSelect() {
+        binding.bnMain.run {
+            setOnItemSelectedListener { item ->
+                when(item.itemId) {
+                    R.id.action_feed -> replaceFragment(FeedFragment())
+                    R.id.action_gallery -> replaceFragment(GalleryFragment())
+                    R.id.action_star -> replaceFragment(StarFragment())
+                }
+                true
+            }
+            selectedItemId = R.id.action_feed
+        }
     }
 }
