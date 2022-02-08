7dee4ad5d97d80cb1bd9abffa2ca4167509942c1
evyooo <yooooojining@gmail.com>
2022-02-09 00:21:07 +0900

    chore: 레이아웃 바인딩  추가

--- app/src/main/java/com/yoo/collectors/FeedFragment.kt
+++ app/src/main/java/com/yoo/collectors/FeedFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.yoo.collectors.databinding.FragmentFeedBinding
 
 
 class FeedFragment : Fragment() {
 
+    private lateinit var binding: FragmentFeedBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_feed, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
 }
