f9fd694b50bcc37b1509cdace999a5c8e889fd2c
evyooo <yooooojining@gmail.com>
2022-02-09 03:12:08 +0900

    feat(image): 카메라 사진 찍고 띄우기

--- app/src/main/java/com/yoo/collectors/FeedFragment.kt
+++ app/src/main/java/com/yoo/collectors/FeedFragment.kt
@@ -1,5 +1,6 @@
 package com.yoo.collectors
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,6 +21,16 @@ class FeedFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_feed, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO delete
+        binding.button.setOnClickListener {
+            val intent = Intent(context, ImageEditActivity::class.java)
+            context?.startActivity(intent)
+        }
     }
 
 }
--- app/src/main/java/com/yoo/collectors/ImageEditActivity.kt
+++ app/src/main/java/com/yoo/collectors/ImageEditActivity.kt
@@ -1,28 +1,50 @@
 package com.yoo.collectors
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.yoo.collectors.databinding.ActivityImageEditBinding
+import com.yoo.collectors.databinding.ActivityMainBinding
 
 class ImageEditActivity : AppCompatActivity() {
 
     val CAMERA = arrayOf(Manifest.permission.CAMERA)
-    val STORAGE = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    val STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
     val CAMERA_CODE = 98
     val STORAGE_CODE = 99
 
+    private lateinit var binding: ActivityImageEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image_edit)
 
+        binding = ActivityImageEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener {
+            callCamera()
+        }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
-    {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode) {
+        when (requestCode) {
             CAMERA_CODE -> {
                 for (grant in grantResults) {
                     if (grant != PackageManager.PERMISSION_GRANTED) {
@@ -39,6 +61,43 @@ class ImageEditActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_CODE -> {
+                    if (data?.extras?.get("data") != null) {
+                        val img = data.extras?.get("data") as Bitmap
+                        binding.imageView.setImageBitmap(img)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkPermission(permissions: Array<out String>): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(this, permissions, CAMERA_CODE)
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private fun callCamera() {
+        if (checkPermission(CAMERA)) {
+            val itt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(itt, CAMERA_CODE)
         }
     }
 }
