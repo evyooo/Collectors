865250d53af900ba84aa5382566bcb3c7b990921
evyooo <yooooojining@gmail.com>
2022-03-27 03:19:46 +0900

    refactor(edit): 카메라 관련 util 코드 정리

--- app/src/main/java/com/yoo/collectors/ImageEditActivity.kt
+++ app/src/main/java/com/yoo/collectors/ImageEditActivity.kt
@@ -1,58 +1,35 @@
 package com.yoo.collectors
 
-import android.Manifest
 import android.app.Activity
-import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.content.res.Resources
 import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.Point
-import android.graphics.RectF
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.DisplayMetrics
 import android.util.Log
-import android.widget.Button
+import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.view.marginRight
-import androidx.lifecycle.Observer
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.yoo.collectors.databinding.ActivityImageEditBinding
-import com.yoo.collectors.model.CropImage
+import com.yoo.collectors.util.CameraUtil
 import com.yoo.collectors.util.Crop
 import com.yoo.collectors.viewmodel.EditViewModel
-import jp.wasabeef.glide.transformations.MaskTransformation
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.io.FileOutputStream
-import java.text.SimpleDateFormat
 
 
 class ImageEditActivity : AppCompatActivity() {
 
-    private val editViewModel: EditViewModel by viewModel()
-
-    val CAMERA = arrayOf(Manifest.permission.CAMERA)
-    val STORAGE = arrayOf(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE
-    )
     val CAMERA_CODE = 98
     val STORAGE_CODE = 99
 
     lateinit var imageList: Array<ImageView>
-    lateinit var maskingPatternList: Array<Int>
+    lateinit var cameraUtil: CameraUtil
 
+    private val editViewModel: EditViewModel by viewModel()
     private lateinit var binding: ActivityImageEditBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,7 +40,34 @@ class ImageEditActivity : AppCompatActivity() {
         binding.viewModel = editViewModel
         setObserver()
         initImages()
+        adjustSize()
 
+        cameraUtil = CameraUtil(this)
+
+    }
+
+    private fun setObserver() {
+        // 뒤로가기
+        editViewModel.closeEvent.observe(this) {
+            onBackPressed()
+        }
+
+        // imageView 클릭시 overlay 보이기
+        editViewModel.showOverlayEvent.observe(this) {
+            binding.overlayClEdit.visibility = View.VISIBLE
+        }
+
+        editViewModel.hideOverlayEvent.observe(this) {
+            binding.overlayClEdit.visibility = View.GONE
+        }
+
+        editViewModel.selectCameraEvent.observe(this) {
+            cameraUtil.callCamera()
+        }
+
+        editViewModel.selectGalleryEvent.observe(this) {
+            cameraUtil.callGallery()
+        }
     }
 
     private fun initImages() {
@@ -71,19 +75,9 @@ class ImageEditActivity : AppCompatActivity() {
             binding.imageView, binding.imageView2, binding.imageView3, binding.imageView4
         )
 
-        // TODO 이거 서버에서 받아오게 바꾸기
-        maskingPatternList = arrayOf(
-            R.drawable.crop1, R.drawable.crop2, R.drawable.crop3, R.drawable.crop4
-        )
         if (editViewModel.editedImageList.isEmpty()) {
-            editViewModel.initImageList(imageList, maskingPatternList)
+            editViewModel.initImageList(imageList)
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        adjustSize()
     }
 
     private fun adjustSize() {
@@ -99,7 +93,7 @@ class ImageEditActivity : AppCompatActivity() {
         val width = sizewidth
 
         val height = width * 3f / 5
-        binding.imageView7.layoutParams.height = height.toInt()
+        binding.backgroundIvEdit.layoutParams.height = height.toInt()
         imageList.forEach { img ->
             val calc = width * 32f / 125
             img.layoutParams.width = calc.toInt()
@@ -117,17 +111,7 @@ class ImageEditActivity : AppCompatActivity() {
         setTopMargin(binding.imageView4, (width * 24f / 1000).toInt())
     }
 
-    private fun setObserver() {
-        // 뒤로가기
-        editViewModel.closeEvent.observe(this) {
-            onBackPressed()
-        }
 
-        // imageView 클릭시 overlay 보이기
-        editViewModel.showOverlayEvent.observe(this) {
-            selectDialog()
-        }
-    }
 
     private fun setRightMargin(imageView: ImageView, int: Int) {
         val layoutParams: ConstraintLayout.LayoutParams = imageView.layoutParams as ConstraintLayout.LayoutParams
@@ -139,31 +123,13 @@ class ImageEditActivity : AppCompatActivity() {
         layoutParams.topMargin = int
     }
 
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            CAMERA_CODE -> {
-                for (grant in grantResults) {
-                    if (grant != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "카메라 권한을 승인해 주세요.", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-
-            STORAGE_CODE -> {
-                for (grant in grantResults) {
-                    if (grant != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "저장소 권한을 승인해 주세요.", Toast.LENGTH_LONG).show()
-                        //finish() 앱을 종료함
-                    }
-                }
-            }
-        }
+        cameraUtil.requestPermissionResult(requestCode, grantResults)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -187,61 +153,4 @@ class ImageEditActivity : AppCompatActivity() {
             }
         }
     }
-
-
-    // TODO 다이얼로그 -> 이미지뷰로 교체 예정
-    fun selectDialog(){
-        val dialog = BottomSheetDialog(this)
-        val view = layoutInflater.inflate(R.layout.dialog_select, null)
-
-        dialog.setContentView(view)
-        dialog.show()
-
-        val camera = view.findViewById<Button>(R.id.camera_btn_select)
-        val gallery = view.findViewById<Button>(R.id.gallery_btn_select)
-
-        camera.setOnClickListener {
-            callCamera()
-            dialog.dismiss()
-            dialog.cancel()
-        }
-
-        gallery.setOnClickListener {
-            getAlbum()
-            dialog.dismiss()
-            dialog.cancel()
-        }
-    }
-
-
-    fun checkPermission(permissions: Array<out String>, type: Int): Boolean {
-        for (permission in permissions) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(this, permissions, type)
-                return false
-            }
-        }
-
-        return true
-    }
-
-    private fun callCamera() {
-        if (checkPermission(CAMERA, CAMERA_CODE) && checkPermission(STORAGE, STORAGE_CODE)) {
-            val itt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(itt, CAMERA_CODE)
-        }
-    }
-
-    fun getAlbum() {
-        if (checkPermission(STORAGE, STORAGE_CODE)) {
-            val itt = Intent(Intent.ACTION_PICK)
-            itt.type = MediaStore.Images.Media.CONTENT_TYPE
-            startActivityForResult(itt, STORAGE_CODE)
-        }
-    }
-
 }
--- app/src/main/java/com/yoo/collectors/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/yoo/collectors/viewmodel/EditViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.yoo.collectors.Event
+import com.yoo.collectors.R
 import com.yoo.collectors.SingleLiveData
 import com.yoo.collectors.model.CropImage
 import com.yoo.collectors.model.EditRepository
@@ -18,6 +19,15 @@ class EditViewModel(private val repository: EditRepository): ViewModel() {
 
     private val _showOverlayEvent = SingleLiveData<Event<Unit>>()
     val showOverlayEvent: LiveData<Event<Unit>> = _showOverlayEvent
+
+    private val _hideOverlayEvent = SingleLiveData<Event<Unit>>()
+    val hideOverlayEvent: LiveData<Event<Unit>> = _hideOverlayEvent
+
+    private val _selectCameraEvent = SingleLiveData<Event<Unit>>()
+    val selectCameraEvent: LiveData<Event<Unit>> = _selectCameraEvent
+
+    private val _selectGalleryEvent = SingleLiveData<Event<Unit>>()
+    val selectGalleryEvent: LiveData<Event<Unit>> = _selectGalleryEvent
 
     // TODO livedata로 교체필요
     var editedImageList = arrayListOf<CropImage>()
@@ -32,9 +42,26 @@ class EditViewModel(private val repository: EditRepository): ViewModel() {
         selected = i
     }
 
+    fun onHideOverlayClick() {
+        _hideOverlayEvent.value = Event(Unit)
+    }
+
+    fun onSelectClick(isCamera: Boolean) {
+        if (isCamera) {
+            _selectCameraEvent.value = Event(Unit)
+        }
+        else {
+            _selectGalleryEvent.value = Event(Unit)
+        }
+        _hideOverlayEvent.value = Event(Unit)
+    }
+
     // TODO 여기 나중에 Room에서 가져오는걸로 교체
-    // pattern은 서버에서 받아올듯
-    fun initImageList(imgList: Array<ImageView>, patternList: Array<Int>) {
+    // pattern은 서버에서 받아올듯 background도..
+    fun initImageList(imgList: Array<ImageView>) {
+        val patternList = arrayOf(
+            R.drawable.crop1, R.drawable.crop2, R.drawable.crop3, R.drawable.crop4
+        )
         for (i in imgList.indices) {
             editedImageList.add(CropImage(imgList[i], null, patternList[i]))
         }
